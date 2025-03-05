063c22edfafa6622ec08737e2b43b8aa11d1e049
juicyg6 <garland.cheang@billigence.com>
2025-03-06 10:43:32 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie!:cup with straw")
