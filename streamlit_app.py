6a42dbf665c03a05b84d6c230c0ea735dec2f77a
juicyg6 <garland.cheang@billigence.com>
2025-03-06 08:21:16 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,3 @@ if ingredients_list:
 
 if session is None:
     st.error("❌ Snowflake session is not active. Check connection settings.")
-
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
